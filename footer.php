1733c09b37e12b58f70dd0359553a7fda2839728
joshmarin <162035475+joshmarin@users.noreply.github.com>
2024-04-18 16:27:21 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,17 +3,16 @@
 <div class="row">
 <div class="col-md-6">
   <a class="navbar-brand" href="#"><span class="josh">JOSH</span><span class="orange">WHITKIN</span></a>
-<p></p>
 
 </div>
-
 <div class="col-md-6">
     <p>Terms and Conditions</p>
     
     </div>
 
-
 </div> <!-- row -->  
 </div> <!-- container -->
     </footer>
     <?php wp_footer(); ?>
+</body>
+</html>
